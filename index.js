fe5f3cceaec6091d28c14b015ff170e8d4f62a1f
Sinésio Neto <sn.sineto@gmail.com>
2020-07-31 11:04:39 -0300

    feat: add express routers

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const router = require('./src/routers');
+
 const app = express();
 
 app.use(bodyParser.json());
+app.use(router);
 
 app.get('/', (req, res) => {
   res.send({ message: 'hello there!' });
