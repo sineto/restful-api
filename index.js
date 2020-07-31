6338f602e9352e7820e6ae805dba85ed51b7a42a
Sinésio Neto <sn.sineto@gmail.com>
2020-07-31 16:06:43 -0300

    chore: add some comments

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const errorHandler = require('./src/middlewares/error-handler');
 
+const errorHandler = require('./src/middlewares/error-handler');
 const router = require('./src/routers');
 
 const app = express();
 
+// parse requests of content-type - application/json
 app.use(bodyParser.json());
+
+// global error handler
 app.use(errorHandler);
+
+// global router
 app.use(router);
 
 app.get('/', (req, res) => {
